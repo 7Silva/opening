7c13bb9c353da99a4c63eb7fc923b740f0326bbe
Silva <7Silva>
2024-04-08 18:30:29 -0300

    :memo: docs: swagger types integration

--- handler/response.go
+++ handler/response.go
@@ -4,21 +4,42 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/7Silva/openings/schemas"
 	"github.com/gin-gonic/gin"
 )
 
-func sendError(ctx *gin.Context, code int, msg string) {
-	ctx.Header("Content-type", "application/json")
+func sendResponse(ctx *gin.Context, code int, message string, data interface{}) {
 	ctx.JSON(code, gin.H{
-		"message":   msg,
-		"errorCode": code,
+		"message": message,
+		"data":    data,
 	})
 }
 
-func sendSuccess(ctx *gin.Context, op string, data interface{}) {
-	ctx.Header("Content-type", "application/json")
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": fmt.Sprintf("Operation from handler: %s successfull", op),
-		"data":    data,
-	})
+func sendError(ctx *gin.Context, code int, message string) {
+	sendResponse(ctx, code, message, nil)
+}
+
+func sendSuccess(ctx *gin.Context, operation string, data interface{}) {
+	message := fmt.Sprintf("Operation from handler: %s successful", operation)
+	sendResponse(ctx, http.StatusOK, message, data)
+}
+
+type ErrorResponse struct {
+	Message   string `json:"message"`
+	ErrorCode int    `json:"errorCode"`
+}
+
+type DeleteOpeningResponse struct {
+	Data    string `json:"data"`
+	Message string `json:"message"`
+}
+
+type OpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+
+type GetOpeningsResponse struct {
+	Message string                    `json:"message"`
+	Data    []schemas.OpeningResponse `json:"data"`
 }
