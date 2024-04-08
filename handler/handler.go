6b5d9a058b74582761afcb898b102c4703aa44d4
Silva <7Silva>
2024-04-08 18:28:20 -0300

    :truck: chore: migrating initializations to dbFunctions.go

--- handler/handler.go
+++ handler/handler.go
@@ -1,16 +1,1 @@
 package handler
-
-import (
-	"github.com/7Silva/openings/config"
-	"gorm.io/gorm"
-)
-
-var (
-	logger *config.Logger
-	db     *gorm.DB
-)
-
-func InitHandler() {
-	logger = config.GetLogger("handler")
-	db = config.GetSQLite()
-}
