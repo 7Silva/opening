9a58522bc9df5f33bebc2cac615844b31c0fad51
Silva <7Silva>
2024-04-08 18:29:11 -0300

    :sparkles: feat: add UpdateOpeningRequest validate

--- handler/request.go
+++ handler/request.go
@@ -2,11 +2,16 @@ package handler
 
 import "fmt"
 
-func errParamIsRequired(name, typ string) error {
-	return fmt.Errorf("param: %s (type: %s)", name, typ)
+type CreateOpeningRequest struct {
+	Role     string `json:"role" binding:"required"`
+	Company  string `json:"company" binding:"required"`
+	Location string `json:"location" binding:"required"`
+	Remote   *bool  `json:"remote" binding:"required"`
+	Link     string `json:"link" binding:"required"`
+	Salary   int64  `json:"salary" binding:"required"`
 }
 
-type CreateOpeningRequest struct {
+type UpdateOpeningRequest struct {
 	Role     string `json:"role"`
 	Company  string `json:"company"`
 	Location string `json:"location"`
@@ -16,28 +21,41 @@ type CreateOpeningRequest struct {
 }
 
 func (r *CreateOpeningRequest) Validate() error {
-	if r.Role == "" && r.Company == "" && r.Location == "" && r.Link == "" && r.Remote == nil && r.Salary <= 0 {
-		return fmt.Errorf("request body is empty or malformed")
-	}
-
 	if r.Role == "" {
 		return errParamIsRequired("role", "string")
 	}
+
 	if r.Company == "" {
 		return errParamIsRequired("company", "string")
 	}
+
 	if r.Location == "" {
 		return errParamIsRequired("location", "string")
 	}
-	if r.Link == "" {
-		return errParamIsRequired("link", "string")
-	}
+
 	if r.Remote == nil {
 		return errParamIsRequired("remote", "bool")
 	}
-	if r.Salary <= 0 {
+
+	if r.Link == "" {
+		return errParamIsRequired("link", "string")
+	}
+
+	if r.Salary == 0 {
 		return errParamIsRequired("salary", "int64")
 	}
 
 	return nil
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary != 0 {
+		return nil
+	}
+
+	return fmt.Errorf("at least one field must be provided")
+}
+
+func errParamIsRequired(name, typ string) error {
+	return fmt.Errorf("param: %s (type: %s)", name, typ)
 }
