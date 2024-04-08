9b980684a173aa92479b5c2ea5c5b78977567721
Silva <7Silva>
2024-04-08 18:21:43 -0300

    :sparkles: feat: add get openings handler logic

--- handler/getOpenings.go
+++ handler/getOpenings.go
@@ -3,11 +3,32 @@ package handler
 import (
 	"net/http"
 
+	"github.com/7Silva/openings/functions"
 	"github.com/gin-gonic/gin"
 )
 
+// @BasePath /v1
+
+// @Summary Get all openings
+// @Description Get all openings
+// @Tags openings
+// @Accept json
+// @Produce json
+// @Success 200 {object} GetOpeningsResponse
+// @Failure 404 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings [get]
 func GetOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GetOpeningsHandler",
-	})
+	openings, err := functions.GetOpenings()
+	if err != nil {
+		sendError(ctx, http.StatusInternalServerError, "Error getting openings from database")
+		return
+	}
+
+	if len(openings) == 0 {
+		sendError(ctx, http.StatusNotFound, "No openings found")
+		return
+	}
+
+	sendSuccess(ctx, "getOpenings", openings)
 }
