e829a31ca25025f068e4fdcfd901c64bee7f784b
Silva <7Silva>
2024-04-08 18:21:56 -0300

    :sparkles: feat: add update opening handler logic

--- handler/updateOpening.go
+++ handler/updateOpening.go
@@ -2,12 +2,46 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/7Silva/openings/functions"
+	"github.com/7Silva/openings/schemas"
 	"github.com/gin-gonic/gin"
 )
 
+// @BasePath /v1
+
+// @Summary Update an opening
+// @Description Update an opening
+// @Tags openings
+// @Accept json
+// @Produce json
+// @Param id path string true "Opening ID"
+// @Param opening body UpdateOpeningRequest true "Opening data to Update"
+// @Success 200 {object} OpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings/{id} [put]
 func UpdateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "UpdateOpeningHandler",
-	})
+	id := ctx.Param("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, "ID is required")
+		return
+	}
+
+	var opening schemas.Opening
+	idUint, _ := strconv.ParseUint(id, 10, 64)
+	if err := ctx.ShouldBindJSON(&opening); err != nil {
+		sendError(ctx, http.StatusBadRequest, "Invalid JSON provided")
+		return
+	}
+
+	opening.ID = uint(idUint)
+	updatedOpening, err := functions.UpdateOpening(opening)
+	if err != nil {
+		sendError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	sendSuccess(ctx, "updateOpening", updatedOpening)
 }
